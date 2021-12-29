804e3cff8212b6c396857efa61ea5059394e914c
Matias <Matias@tvorup.eu>
2021-12-30 00:06:56 +0100

    Working on infeciton in voiceChannelHandler.py. Lot of loose ends.

--- src/discordBotModule/botStartup.py
+++ src/discordBotModule/botStartup.py
@@ -5,4 +5,5 @@ import discord
 def setupBot(client):
   client = messageHandler.bindCommands(client)
   client = voiceChannelHandler.bindVoiceEvents(client)
+  #Check om der er nogen inficerede personer i serveren? Hvis nej inficer en admin
   return client
--- src/discordBotModule/voiceChannelHandler.py
+++ src/discordBotModule/voiceChannelHandler.py
@@ -1,4 +1,5 @@
 import discordBotModule
+from mongoDbModule import mongoHandler
 
 def bindVoiceEvents(client):
   @client.event
@@ -8,6 +9,15 @@ def bindVoiceEvents(client):
       #Hent liste af alle brugere i kanalen.
       memberIds = memberListToIdList(after.channel.members)
       #Check om nogen af dem er inficeret. #Åh nej, det er jo database ting.
+      memberObjectList = mongoHandler.getUserList(member.guild.id, memberIds)
+      if(not channelContainsInfectedUsers(memberObjectList)):
+        return
+      #Get an infected user
+      
+      uninfectedUserIds = getUninfectedUserIds(memberObjectList)
+      
+
+
       #Hvis der er en inficeret bruger: inficér alle ikke-inficerede brugere.
       print("etwas")
 
@@ -19,3 +29,20 @@ def memberListToIdList(members):
   for member in members:
     list.append(member.id)
   return list
+
+def channelContainsInfectedUsers(memberList):
+  for member in memberList:
+    if member["Infected"] == True:
+      return True
+  return False
+
+def getUninfectedUserIds(memberList):
+  list = []
+  for member in memberList:
+    if(member["Infected"==False]):
+      list.append(member["UserID"])
+  return list
+
+# def infectMultipleIds(discordServerId, infectedUserIds, infectedByUserId):
+#   for id in IDList:
+#     mongoHandler.infectUser()
